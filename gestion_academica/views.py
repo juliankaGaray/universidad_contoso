5df4c73b75310332871b8e1dac7e01132821dac6
Jennifer Zamora <165498055+jenny1703@users.noreply.github.com>
2025-02-13 09:38:23 -0500

    Update views.py

--- gestion_academica/views.py
+++ gestion_academica/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Curso
+from .serializers import CursoSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def lista_cursos(request):
+    if request.method == 'GET':
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = CursoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
